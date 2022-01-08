221d4849659aea1d2bc2eddf4417c9ed128391e7
Demetra211 <desmonddleema211@gmail.com>
2022-01-08 18:43:05 +0530

    Overlay for mobile menu

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const btnhamburger = document.querySelector("#btnhamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnhamburger.addEventListener('click', function(){
+    console.log("Hamburger clicked");
+    if(header.classList.contains("open")){
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    }
+    else{
+        header.classList.add("open");
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+})
